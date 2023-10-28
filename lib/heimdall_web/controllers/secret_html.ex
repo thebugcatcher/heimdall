952863bca63596a5b23604e0bca4ecdf115e184d
Adi Iyengar <aditya7iyengar@gmail.com>
2023-10-28 17:42:04 -0400

    Add better secret creation interface

--- lib/heimdall_web/controllers/secret_html.ex
+++ lib/heimdall_web/controllers/secret_html.ex
@@ -11,20 +11,21 @@ defmodule HeimdallWeb.SecretHTML do
     datetime = DateTime.utc_now()
 
     [
-      {"5 min", Timex.add(datetime, Duration.from_minutes(5))},
       {"15 min", Timex.add(datetime, Duration.from_minutes(15))},
       {"30 min", Timex.add(datetime, Duration.from_minutes(30))},
       {"1 hour", Timex.add(datetime, Duration.from_hours(1))},
-      {"2 hours", Timex.add(datetime, Duration.from_hours(2))}
+      {"5 hours", Timex.add(datetime, Duration.from_hours(5))},
+      {"1 day", Timex.add(datetime, Duration.from_days(1))},
+      {"2 days", Timex.add(datetime, Duration.from_days(2))}
     ]
   end
 
   defp humanize_encryption_algo(:aes_gcm) do
-    "AES GCM (one key to both encrypt and decrypt)"
+    "AES GCM (same password to both encrypt and decrypt)"
   end
 
   defp humanize_encryption_algo(:plaintext) do
-    "Plaintext (no key required)"
+    "Plaintext (no password/key required)"
   end
 
   defp humanize_encryption_algo(:rsa) do
