5e05e86ee254be5ae2515dba0ea8bd75a101b007
Adi Iyengar <aditya7iyengar@gmail.com>
2023-10-29 19:47:59 -0400

    Last bit security-related configs

--- config/config.exs
+++ config/config.exs
@@ -50,6 +50,9 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+# Security
+config :phoenix, :filter_parameters, ["password", "secret", "encrypted_text"]
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
